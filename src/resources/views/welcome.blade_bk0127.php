3a4c5a9f0361cfc893442670a8a33d9526480d3c
masataka-kun <loveandfree0619@gmail.com>
2020-01-29 20:41:38 +0900

    色々追加

--- src/resources/views/welcome.blade_bk0127.php
+++ src/resources/views/welcome.blade_bk0127.php
@@ -87,16 +87,6 @@
                 Skele-Nate
             </div>
 
-            <div class="sub_titele">
-                <h1>骨格診断について</h1>
-                <p>気に入って買ったはずなのに自分の服装を見て、「なんだか似合っていない」「なんとなくだらしなく見える」と思った経験は誰しもあるはず。<br>
-                    その理由は、柄や色などのデザインはさておいて、自分の骨格と合っていないからです。<br>
-                    骨格にあったスタイルを取り入れることで今後買い物のときの失敗も格段に減るはずです。マネキン買いも失敗が減るのでは・・・<br>
-                    大きくナチュラル・ウェーブ・ストレートと3つの骨格タイプに分類されます。分からない方は「チェック」をクッリクして確認してみて下さい。<br>
-                    骨格タイプの分かる方はそれぞれのタイプのコーディネートを参考に
-                </p>
-            </div>
-
             <div class="links">
                 <a href="https://laravel.com/docs">Docs</a>
                 <a href="https://laracasts.com">Laracasts</a>
@@ -107,31 +97,6 @@
                 <a href="https://vapor.laravel.com">Vapor</a>
                 <a href="https://github.com/laravel/laravel">GitHub</a>
             </div>
-        </div>
-
-        <div class="straight_box">
-            <h1 class="straight_title">ストレート型</h1>
-            <h2 class="straight_desc"></h2>
-
-            <div class="straight_image">
-                <div class="straight_img_file1">
-                    <img class="img_file" src="" alt="">
-                </div>
-                <div class="straight_img_file2">
-                    <img class="img_file" src="" alt="">
-                </div>
-                <div class="straight_img_file3">
-                    <img class="img_file" src="" alt="">
-                </div>
-            </div>
-        </div>
-
-        <div class="wave_box">
-
-        </div>
-
-        <div class="natural_boox">
-
         </div>
     </div>
 </body>
