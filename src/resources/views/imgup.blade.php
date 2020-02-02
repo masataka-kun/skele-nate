a1aa088bbf3a0ba1851dafe507338013aeb9aaa4
masataka-kun <loveandfree0619@gmail.com>
2020-02-02 15:16:41 +0900

    img up 追加

--- src/resources/views/imgup.blade.php
+++ src/resources/views/imgup.blade.php
@@ -1,41 +1,4 @@
-<!-- エラーメッセージ。なければ表示しない -->
-@if ($errors->any())
-<ul>
-    @foreach($errors->all() as $error)
-    <li>{{ $error }}</li>
-    @endforeach
-</ul>
-@endif
-
 <!-- フォーム -->
-<!-- <form action="{{ url('upload') }}" method="POST" enctype="multipart/form-data">
-     @csrf -->
-<!-- アップロードした画像。なければ表示しない -->
-<!-- @isset ($filename) -->
-<!-- <div>
-        <img src="{{ asset('storage/' . $filename) }}">
-    </div>
-    @endisset -->
-
-<!-- <label for="photo">画像ファイル:</label>
-    <input type="file" class="form-control" name="file">
-    <br>
-    <hr>
-     {{ csrf_field() }} 
-    <button class="btn btn-success"> Upload </button> -->
-<!-- </form> -->
-<!-- <form action="/bbs" method="POST" enctype="multipart/form-data" class="post_form">
-    <img src="data:image/png;base64,<?= image ?>">
-    <div class="form_parts">
-        <input type="file" name="image">
-        <br>
-        <br>
-        <textarea name="comment" rows="4" cols="40"></textarea>
-        <br>
-        {{ csrf_field() }}
-        <button class="btn btn-success">投稿</button>
-    </div>
-</form> -->
 <form method="POST" action="{{ route('image.store') }}" enctype="multipart/form-data">
     @csrf
     <input id="image" type="file" name="image">
